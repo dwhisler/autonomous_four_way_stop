f5b074a408ab88cb9c5966a57d2bcba4e43d038e
Nick Comly <ncomly@stanford.edu>
2020-11-05 10:56:44 -0800

    updated main for different smoothing functions

--- main.py
+++ main.py
@@ -6,6 +6,28 @@ import numpy
 import os
 import matplotlib.pyplot as plt
 
+################################################################################
+# Plotting Helpers
+################################################################################
+def smooth(scalars: List[float], weight: float) -> List[float]:  # Weight between 0 and 1
+    last = scalars[0]  # First value in the plot (first timestep)
+    smoothed = list()
+    for point in scalars:
+        smoothed_val = last * weight + (1 - weight) * point  # Calculate smoothed value
+        smoothed.append(smoothed_val)                        # Save it
+        last = smoothed_val                                  # Anchor the last smoothed value
+
+    return smoothed
+
+def moving_average(a, n=100) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+
+################################################################################
+# MDP Creation Helpers
+################################################################################
 
 def create_grid(n):
     grid = np.zeros((n,n))
@@ -23,6 +45,11 @@ def get_stops(n):
     stops.append((n//2-1, n//2+1))
     return stops
 
+
+################################################################################
+# Main
+################################################################################
+
 def main():
     # gird size
     n = 6
@@ -33,16 +60,19 @@ def main():
     mdp = FourWayStopMDP(grid, stops)
     qRL = QLearningAgent(mdp.actions, mdp.discount, identityFeatureExtractor_str, )
 
-    total_rewards, crashes, visualization = simulate(mdp, qRL, maxIterations=10, numTrials=100000)
+    total_rewards, crashes, visualization = simulate(mdp, qRL, maxIterations=10, numTrials=10000)
 
-    def moving_average(a, n=100) :
-        ret = np.cumsum(a, dtype=float)
-        ret[n:] = ret[n:] - ret[:-n]
-        return ret[n - 1:] / n
+
 
     # plt.plot(moving_average(total_rewards))
     # plt.plot(total_rewards)
-    plt.plot(moving_average(crashes, 500))
+    # plt.plot(total_rewards, label='base')
+    plt.plot(smooth(total_rewards, .9), label='.9')
+    # plt.plot(smooth(total_rewards, .8), label='.8')
+    # plt.plot(smooth(total_rewards, .7), label='.7')
+    # plt.plot(smooth(total_rewards, .6), label='.6')
+    plt.plot(moving_average(total_rewards, n=20), label='mv 20')
+    plt.legend()
     plt.show()
     # print(list(visualization[0]))
 
